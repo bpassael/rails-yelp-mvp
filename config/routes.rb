be36e22e2d4f543e4538b80a6bd7d23fa1f3e6e7
Bastien Perez Assael <perez.bas@gmail.com>
2022-08-17 14:25:00 +0200

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:index, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Restaurant.create(name: "Chez Vann", address: "17 Bd Joseph Garnier, 06000 Nice", phone_number: "04 93 98 47 47", category: "chinese")
+Restaurant.create(name: "La Casa di Giulia", address: "2 Rue Flaminius Raiberti", phone_number: "09 53 47 59 85", category: "italian")
+Restaurant.create(name: "Sushiya", address: "16 Rue Biscarra, 06000 Nice", phone_number: "04 93 13 97 18", category: "japanese")
+Restaurant.create(name: "Bistrot Gourmand", address: "3 Rue Desboutin, 06300 Nice", phone_number: "04 92 14 55 55", category: "french")
+Restaurant.create(name: "FRITES CITY", address: "21 Rue Delille, 06000 Nice", phone_number: "09 54 01 86 68", category: "belgian")
